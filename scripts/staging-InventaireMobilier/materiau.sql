523af9a015e2aac77d58d5c2116aacd977e23e67
Loana <loana.babey@heig-vd.ch>
2026-03-27 11:07:13 +0100

    feat: filtrage colonne materiau

--- scripts/staging-InventaireMobilier/materiau.sql
+++ scripts/staging-InventaireMobilier/materiau.sql
@@ -1,28 +1,17 @@
--- Active: 1773394971258@@127.0.0.1@5432@yverdon-inventory@staging
 -- Active: 1773394971258@@127.0.0.1@5432@yverdon-inventory
-
+-- Active: 1773394971258@@127.0.0.1@5432@yverdon-inventory
 
 --Aperçu colonne materiau
 SELECT materiau, COUNT(*) AS nb
 FROM inventaire_mobilier
-GROUP BY materiau
+GROUP BY
+    materiau
 ORDER BY nb DESC;
 
-DELETE FROM inventaire_mobilier
-WHERE TRIM(materiau) = '' OR materiau IS NULL;
+CREATE EXTENSION IF NOT EXISTS unaccent;
 
-UPDATE inventaire_mobilier
-SET materiau = CASE LOWER(TRIM(materiau))
-    WHEN 'bois'         THEN 'bois'
-    WHEN 'Métal'  THEN 'métal'
-    WHEN 'metal'        THEN 'métal'
-    WHEN 'sodium'    THEN 'sodium'
-    WHEN 'LED'          THEN 'led'
-    WHEN 'pierre'        THEN 'pierre'
-    WHEN 'Pierre'        THEN 'pierre'
-    WHEN 'béton'          THEN 'béton'
-    ELSE LOWER(TRIM(materiau))  -- minuscules + trim pour tout le reste
-END;
-
-
-
+SELECT DISTINCT
+    unaccent (LOWER(materiau))
+FROM inventaire_mobilier
+WHERE
+    materiau IS NOT NULL;
