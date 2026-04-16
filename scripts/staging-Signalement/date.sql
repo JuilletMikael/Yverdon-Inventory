083eccf38fcc13f281b1b2e6f427790555f55742
LauraFabb <laura.fabbri@heig-vd.ch>
2026-04-16 09:59:53 +0200

    modification des dates sous ce format: YYYY-MM-DD

--- scripts/staging-Signalement/date.sql
+++ scripts/staging-Signalement/date.sql
@@ -8,17 +8,19 @@ ORDER BY nb DESC;
 SELECT COUNT(*) as total_lignes
 FROM signalements;
 
--- Convertir TOUS les formats de dates en YYYY-MM-DD
-UPDATE staging.signalements
-SET "date" = 
-  CASE
-    WHEN "date" ~ '^\d{4}-\d{2}-\d{2}$' THEN TO_CHAR(TO_DATE("date", 'DD.MM.YYYY'), 'YYYY-MM-DD')
-    WHEN "date" ~ '^\d{2}\.\d{2}\.\d{4}$' THEN TO_CHAR(TO_DATE("date", 'YYYY-MM-DD'), 'YYYY-MM-DD')
-  END
-WHERE "date" IS NOT NULL;
 
+SELECT "date"
+FROM staging.signalements
+WHERE "date" ~ '^\d{2}\.\d{2}\.\d{4}$';
+
+
+UPDATE staging.signalements
+SET "date" = TO_CHAR(
+    TO_DATE("date", 'DD.MM.YYYY'),
+    'YYYY-MM-DD'
+)
+WHERE "date" ~ '^\d{2}\.\d{2}\.\d{4}$';
 
 SELECT DISTINCT "date"
 FROM staging.signalements
 ORDER BY "date";
-
