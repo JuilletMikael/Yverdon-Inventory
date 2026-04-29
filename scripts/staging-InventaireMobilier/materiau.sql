1ee89052806f2bfcfbeb6e4e4fd522d44088bce0
JuilletMikael <juillet.mikael@gmail.com>
2026-04-29 19:29:54 +0200

    refactor(materiau): remove select not usefull

--- scripts/staging-InventaireMobilier/materiau.sql
+++ scripts/staging-InventaireMobilier/materiau.sql
@@ -1,10 +1,3 @@
---Aperçu colonne materiau
-SELECT materiau, COUNT(*) AS nb
-FROM staging.inventaire_mobilier
-GROUP BY
-    materiau
-ORDER BY nb DESC;
-
 CREATE EXTENSION IF NOT EXISTS unaccent;
 
 INSERT INTO public.materiaux (libelle)
