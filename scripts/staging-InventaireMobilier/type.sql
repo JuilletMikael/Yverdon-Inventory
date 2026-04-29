7727bc5ab9f31292156813a8f540f40f084df50e
JuilletMikael <juillet.mikael@gmail.com>
2026-04-29 19:32:40 +0200

    refactor(types_inventaires): remove select unused and correct spacing

--- scripts/staging-InventaireMobilier/type.sql
+++ scripts/staging-InventaireMobilier/type.sql
@@ -1,21 +1,15 @@
-SELECT type, COUNT(*) AS nb
-FROM staging.inventaire_mobilier
-GROUP BY
-    type
-ORDER BY nb DESC;
-
-INSERT INTO public.types_inventaires (libelle)
+INSERT INTO
+    public.types_inventaires (libelle)
 SELECT DISTINCT
-            CASE
-            WHEN LOWER(TRIM(type)) LIKE '%banc%' THEN 'banc'
-            WHEN LOWER(TRIM(type)) LIKE '%lampadaire%' THEN 'lampadaire'
-            WHEN LOWER(TRIM(type)) LIKE '%poubelle%' THEN 'poubelle'
-            WHEN LOWER(TRIM(type)) LIKE '%corbeille%' THEN 'corbeille'
-            WHEN LOWER(TRIM(type)) LIKE '%fontaine%' THEN 'fontaine'
-            WHEN LOWER(TRIM(type)) LIKE '%borne%' THEN 'borne EV'
-            WHEN LOWER(TRIM(type)) LIKE '%panneau%' THEN 'panneau'
-            END
-
+    CASE
+        WHEN LOWER(TRIM(type)) LIKE '%banc%' THEN 'banc'
+        WHEN LOWER(TRIM(type)) LIKE '%lampadaire%' THEN 'lampadaire'
+        WHEN LOWER(TRIM(type)) LIKE '%poubelle%' THEN 'poubelle'
+        WHEN LOWER(TRIM(type)) LIKE '%corbeille%' THEN 'corbeille'
+        WHEN LOWER(TRIM(type)) LIKE '%fontaine%' THEN 'fontaine'
+        WHEN LOWER(TRIM(type)) LIKE '%borne%' THEN 'borne EV'
+        WHEN LOWER(TRIM(type)) LIKE '%panneau%' THEN 'panneau'
+    END
 FROM staging.inventaire_mobilier
 WHERE
     type IS NOT NULL;
