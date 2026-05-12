a95885c34b309479618cf955c225b049e7445f4a
JuilletMikael <juillet.mikael@gmail.com>
2026-05-12 11:50:42 +0200

    feat: add technicien with data given

--- scripts/staging-Intervention/techniciens.sql
+++ scripts/staging-Intervention/techniciens.sql
@@ -21,3 +21,13 @@ SELECT DISTINCT
             THEN ''
     END AS nom
 FROM staging.interventions;
+
+INSERT INTO public.techniciens (prenom, nom, telephone, email) VALUES
+('Jean-Marc', 'Bonvin', '024 423 55 01', 'jm.bonvin@yverdon.ch'),
+('Pedro', 'Alves', '024 423 55 02', 'p.alves@yverdon.ch'),
+('Marc', 'Koffi', '024 423 55 03', 'm.koffi@yverdon.ch');
+
+
+Insert INTO public.techniciens (prenom, nom, telephone, email)
+SELECT DISTINCT nom, prenom, telephone, email
+FROM staging.techniciens;
