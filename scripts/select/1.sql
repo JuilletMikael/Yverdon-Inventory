40b31b5ba610cc55337975f8e8584b2820fb34e0
JuilletMikael <juillet.mikael@gmail.com>
2026-05-27 19:41:55 +0200

    feat : try correct intervention not adding intervention

---
+++ scripts/select/1.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE VIEW public.v_cout_par_type_mobilier AS
+SELECT
+
+    EXTRACT(YEAR FROM inter.date) AS annee,
+
+    ti.libelle AS type_mobilier,
+
+    COUNT(DISTINCT inter.id) AS nombre_interventions,
+
+    COALESCE(SUM(f.cout_total), 0) AS cout_total,
+
+    ROUND(
+        COALESCE(AVG(f.cout_total), 0),
+        2
+    ) AS cout_moyen_par_intervention
+
+FROM public.interventions inter
+
+LEFT JOIN public.interventions_factures ifac
+    ON ifac.id_interventions = inter.id
+
+LEFT JOIN public.factures f
+    ON f.id = ifac.id_factures
+
+INNER JOIN public.tickets t
+    ON t.id = inter.id_tickets
+
+INNER JOIN public.inventaires inv
+    ON inv.id = t.id_inventaire
+
+INNER JOIN public.types_inventaires ti
+    ON ti.id = inv.id_types_inventaires
+
+WHERE EXTRACT(YEAR FROM inter.date) IN (2024, 2025)
+
+GROUP BY
+    annee,
+    ti.libelle
+
+ORDER BY
+    annee,
+    cout_total DESC;
--- scripts/staging-Intervention/interventions.sql
+++ scripts/staging-Intervention/interventions.sql
@@ -8,61 +8,120 @@ INSERT INTO public.interventions (
 )
 
 SELECT DISTINCT
+
     CASE
         WHEN s.date ~ '^\d{2}\.\d{2}\.\d{4}$'
             THEN TO_DATE(s.date, 'DD.MM.YYYY')
+
         WHEN s.date ~ '^\d{4}-\d{2}-\d{2}$'
             THEN TO_DATE(s.date, 'YYYY-MM-DD')
+
         WHEN s.date ~ '^\d{2}/\d{2}/\d{4}$'
             THEN TO_DATE(s.date, 'DD/MM/YYYY')
+
         ELSE NULL
     END AS date_uniformisee,
+
     CASE
-    WHEN s.duree ~ '^\d+\s*(min|mn)$'
-        THEN CAST(REGEXP_REPLACE(s.duree, '\D', '', 'g') AS INTEGER)
 
-    WHEN s.duree ~ '^\d+(\.\d+)?\s*h$'
-        THEN ROUND(CAST(REGEXP_REPLACE(s.duree, '[^0-9\.]', '', 'g') AS NUMERIC) * 60)
+        WHEN s.duree ~ '^\d+\s*(min|mn)$'
+            THEN CAST(REGEXP_REPLACE(s.duree, '\D', '', 'g') AS INTEGER)
 
-    WHEN s.duree ~ '^\\d+h\\d{2}$'
-        THEN (
-            CAST(REGEXP_REPLACE(s.duree, 'h.*', '') AS INTEGER) * 60
-            +
-            CAST(REGEXP_REPLACE(s.duree, '.*h', '') AS INTEGER)
-        )
+        WHEN s.duree ~ '^\d+(\.\d+)?\s*h$'
+            THEN ROUND(
+                CAST(
+                    REGEXP_REPLACE(
+                        s.duree,
+                        '[^0-9\.]',
+                        '',
+                        'g'
+                    ) AS NUMERIC
+                ) * 60
+            )
+
+        WHEN s.duree ~ '^\d+h\d{2}$'
+            THEN (
+                CAST(REGEXP_REPLACE(s.duree, 'h.*', '') AS INTEGER) * 60
+                +
+                CAST(REGEXP_REPLACE(s.duree, '.*h', '') AS INTEGER)
+            )
 
         WHEN LOWER(s.duree) = 'une matinée'
             THEN 240
 
         WHEN LOWER(s.duree) = 'une journée'
             THEN 480
+
         ELSE NULL
+
     END AS duree_minute,
+
     s.remarques,
+
     tech.id,
+
     tk.id,
+
     ti.id
+
 FROM staging.interventions s
 
-JOIN staging.signalements sig
-    ON sig.objet = s.objet
+LEFT JOIN staging.signalements sig
+    ON LOWER(TRIM(public.unaccent(sig.objet)))
+    =
+    LOWER(TRIM(public.unaccent(s.objet)))
 
-INNER JOIN public.techniciens tech
-    ON SPLIT_PART(s.technicien, ' ', 1) = tech.prenom
+LEFT JOIN public.techniciens tech
 
-INNER JOIN public.tickets tk
-    ON tk.id_urgences = (
-        SELECT u.id
-        FROM public.urgences u
-        WHERE LOWER(TRIM(sig.urgence)) LIKE '%' || u.libelle || '%'
-        LIMIT 1
-    )
-    AND tk.id_etats_tickets = (
-        SELECT e.id
-        FROM public.etats_tickets e
-        WHERE LOWER(TRIM(sig.statut)) LIKE '%' || e.libelle || '%'
-        LIMIT 1
-    )
+ON tech.nom = CASE
+    WHEN s.technicien ILIKE '%JM%' THEN 'Bonvin'
+    WHEN s.technicien ILIKE '%Jean-Marc%' THEN 'Bonvin'
+    WHEN s.technicien ILIKE '%Pedro%' THEN 'Alves'
+    WHEN s.technicien ILIKE '%P. Alves%' THEN 'Alves'
+    WHEN s.technicien ILIKE '%Alves Pedro%' THEN 'Alves'
+    WHEN s.technicien ILIKE '%Koffi%' THEN 'Koffi'
+    WHEN s.technicien ILIKE '%stagiaire%' THEN 'stagiaire'
+END
+
+INNER JOIN staging.inventaire_mobilier inv_stage
+
+ON
+(
+    CASE
+        WHEN LOWER(s.objet) LIKE '%banc%' THEN 'banc'
+        WHEN LOWER(s.objet) LIKE '%lampadaire%' THEN 'lampadaire'
+        WHEN LOWER(s.objet) LIKE '%poubelle%' THEN 'poubelle'
+        WHEN LOWER(s.objet) LIKE '%corbeille%' THEN 'corbeille'
+        WHEN LOWER(s.objet) LIKE '%fontaine%' THEN 'fontaine'
+        WHEN LOWER(s.objet) LIKE '%borne%' THEN 'borne'
+        WHEN LOWER(s.objet) LIKE '%panneau%' THEN 'panneau'
+        WHEN LOWER(s.objet) LIKE '%eclairage%' THEN 'eclairage'
+    END
+)
+=
+(
+    CASE
+        WHEN LOWER(inv_stage.type) LIKE '%banc%' THEN 'banc'
+        WHEN LOWER(inv_stage.type) LIKE '%lampadaire%' THEN 'lampadaire'
+        WHEN LOWER(inv_stage.type) LIKE '%poubelle%' THEN 'poubelle'
+        WHEN LOWER(inv_stage.type) LIKE '%corbeille%' THEN 'corbeille'
+        WHEN LOWER(inv_stage.type) LIKE '%fontaine%' THEN 'fontaine'
+        WHEN LOWER(inv_stage.type) LIKE '%borne%' THEN 'borne'
+        WHEN LOWER(inv_stage.type) LIKE '%panneau%' THEN 'panneau'
+        WHEN LOWER(inv_stage.type) LIKE '%eclairage%' THEN 'eclairage'
+    END
+)
+
+AND LOWER(public.unaccent(s.objet))
+LIKE '%' || LOWER(public.unaccent(inv_stage.lieu)) || '%'
+
+INNER JOIN public.inventaires inv
+    ON inv.reference = inv_stage.id
+
+LEFT JOIN public.tickets tk
+    ON tk.id_inventaire = inv.id
 
 INNER JOIN public.types_interventions ti
-    ON LOWER(TRIM(s.type_intervention)) = LOWER(TRIM(ti.libelle));
+    ON LOWER(TRIM(public.unaccent(s.type_intervention)))
+    =
+    LOWER(TRIM(public.unaccent(ti.libelle)));
