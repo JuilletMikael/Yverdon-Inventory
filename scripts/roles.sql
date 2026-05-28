2a9f25c6ab3e0b3c66232b1e51898fb7efa5ca85
Loana <loana.babey@heig-vd.ch>
2026-05-28 12:17:20 +0200

    Ajout des rôles

---
+++ scripts/roles.sql
@@ -0,0 +1,19 @@
+-- Active: 1773394971258@@127.0.0.1@5438@yverdon-inventory
+CREATE ROLE citoyens;
+CREATE ROLE techniciens;
+CREATE ROLE administrateurs;
+
+
+GRANT USAGE ON SCHEMA public TO citoyens;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO citoyens;
+
+GRANT USAGE ON SCHEMA public TO techniciens;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO techniciens;
+
+GRANT USAGE ON SCHEMA public TO administrateurs;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO administrateurs;
+
+SELECT grantee, table_schema, privilege_type
+FROM information_schema.role_table_grants
+WHERE grantee IN ('citoyens', 'techniciens', 'administrateurs')
+ORDER BY grantee, table_name, privilege_type;
